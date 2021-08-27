6d532cbc2b2f33a5ab51c6fe2e8fe05c1faff444
gayathriramanesh <41873717+gayathriramanesh@users.noreply.github.com>
2021-08-27 19:05:13 +0530

    Add files via upload

---
+++ cart.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/mdb.min.css">
+  <link rel="stylesheet" href="./css/addons/datatables.min.css">
+    <style>
+    .footer
+    {
+      position:fixed;
+     padding: 10px 0;
+     background-color: #101010;
+     color:#9d9d9d;
+     bottom:0;
+     width: 100%;
+    }
+    </style>
+    <title>Lifestyle store</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark navbar-fixed-top">
+        <a class="navbar-brand" href="#">Lifestyle store</a>
+    <div class="collapse navbar-collapse " id="navbarNav">
+      <ul class="navbar-nav ml-auto ">
+      <li class="nav-item ">
+      <a class="nav-link " href="cart.htm"><i class="fas fa-cart-plus"></i>cart</a>
+      </li>
+   <li class="nav-item ">
+      <a class="nav-link " href="settings.htm"><i class="fas fa-cog"></i>settings</a>
+    </li>
+    <li class="nav-item ">
+       <a class="nav-link " href="#"><i class="fas fa-sign-out-alt"></i>logout</a>
+     </li>
+  </ul>
+</div>
+</nav>
+<div class="container" style="padding-top:40px">
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">ITEM NO</th>
+      <th scope="col">ITEM NAME</th>
+      <th scope="col">PRICE</th>
+      <th scope="col"></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>TOTAL</td>
+      <td>Rs.0/</td>
+      <td><button type="button" class="btn btn-primary">CONFIRM ORDER</button></td>
+    </tr>
+  </tbody>
+</table>
+</div>
+<div class = "footer">
+  <div class = "container">
+  <center>
+  <p>Copyright © Lifestyle Store. All RightsReserved|Contact Us: +91 90000 00000</p>
+ </center>
+</div>
+</div>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
